1063d14cca6dd5aa0b07aacf9448c9411763a565
Alekya Gadiraju <gadirajualekya@gmail.com>
2021-03-01 15:15:21 -0500

    query 6

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -49,4 +49,16 @@ SELECT * FROM employees
 WHERE first_name = 'Hercules' AND last_name like 'B%';
 
 --6.
+SELECT 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name,
+dept_emp.dept_no
+FROM employees 
+LEFT JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments 
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name='Sales';
+
 
