f46c3536a93b60eb6040c8db0e2c7b3a4a3445e9
Alekya Gadiraju <gadirajualekya@gmail.com>
2021-03-01 15:17:46 -0500

    query 8

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -74,4 +74,10 @@ INNER JOIN departments
 ON departments.dept_no=dept_emp.dept_no
 WHERE departments.dept_name in ('Sales', 'Development')
 
+--8.
+SELECT last_name, COUNT(*) AS freq_count
+FROM employees
+GROUP BY last_name
+ORDER BY freq_count DESC;
+
 
