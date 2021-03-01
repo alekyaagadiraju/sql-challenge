aaf7e77f3ad8c4e007ca1e9296cd886f1d18a910
Alekya Gadiraju <gadirajualekya@gmail.com>
2021-03-01 15:14:33 -0500

    Queries 4 and 5

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,52 @@
+--1. For each employee: employee number, last name, first name, gender, and salary.
+
+SELECT employees.emp_no, 
+employees.last_name,
+employees.first_name,
+employees.gender,
+salaries.salary
+FROM employees
+LEFT JOIN salaries
+ON employees.emp_no = salaries.emp_no
+ORDER BY emp_no
+
+--2. Employees who were hired in 1986.
+
+SELECT * FROM employees
+WHERE DATE_PART('year',hire_date) = 1986
+ORDER BY emp_no;
+
+--3. List the manager: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
+
+SELECT dept_manager.dept_no, 
+departments.dept_name,
+dept_manager.emp_no,
+employees.last_name, 
+employees.first_name,
+dept_manager.from_date,
+dept_manager.to_date
+FROM dept_manager
+LEFT JOIN departments
+ON dept_manager.dept_no = departments.dept_no
+LEFT JOIN employees 
+ON dept_manager.emp_no = employees.emp_no
+ORDER BY emp_no
+
+--4. 
+SELECT
+employees.emp_no,
+employees.last_name,
+employees.first_name,
+dept_emp.dept_no,
+departments.dept_name
+FROM employees 
+INNER JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments ON departments.dept_no=dept_emp.dept_no
+order by emp_no;
+
+--5.
+SELECT * FROM employees
+WHERE first_name = 'Hercules' AND last_name like 'B%';
+
+--6.
+
